0b6004afa82e9b2dbed6277c0c27e0ad41dc6d6b
avilevi <avi@meerkatapp.co>
2016-01-21 08:44:48 +0200

    file structure and naming

--- src/main/scala-2.11/byContext/filteredContainers.scala
+++
@@ -1,4 +0,0 @@
-package byContext
-
-//TODO: review need of this class
-case class Raw(value:Any)
---
+++ src/main/scala-2.11/byContext/valueContainers/RawValueContainer.scala
@@ -0,0 +1,4 @@
+package byContext.valueContainers
+
+//TODO: review need of this class
+case class RawValueContainer(value:Any)
