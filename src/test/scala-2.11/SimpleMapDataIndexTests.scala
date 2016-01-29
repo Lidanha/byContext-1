b00c41deffe1e37eb768653159872dd0f0e7722c
avilevi <avi@meerkatapp.co>
2016-01-29 11:41:56 +0200

    completed tests for SimpleMapDataIndexTests

--- src/test/scala-2.11/SimpleMapDataIndexTests.scala
+++ src/test/scala-2.11/SimpleMapDataIndexTests.scala
@@ -6,5 +6,60 @@ class SimpleMapDataIndexTests extends WordSpecLike  with Matchers{
     "create simple, flat index" in {
       new SimpleMapDataIndex(Map("root"->1)).getItem("root") should be (Some(1))
     }
+    "two roots one flat and one object with one level" in {
+      val index = new SimpleMapDataIndex(Map{
+        "root-1"->1
+        "root-2"->Map{
+          "child1"->1.1
+          "child2"->1.2
+        }
+      })
+
+      index getItem "root-1"  should be (Some(1))
+      index getItem "root-2"  should be (Some(Map{
+        "child1"->1.1
+        "child2"->1.2
+      }))
+      index getItem "root-2.child1"  should be (Some(1.1))
+      index getItem "root-2.child2"  should be (Some(1.2))
+    }
+    "two roots one flat and one object with two levels and one object with three" in {
+      val index = new SimpleMapDataIndex(Map(
+        "1"->"1",
+        "2"->Map(
+          "1"->"2.1",
+          "2"->"2.2"
+      ),
+        "3"->Map(
+          "1"->Map(
+            "1"->"3.1.1",
+            "2"->"3.1.2"
+            ),
+          "2"->"3.2"
+      )
+      ))
+
+      index getItem "1"  should be (Some("1"))
+      index getItem "2"  should be (Some(Map(
+        "1"->"2.1",
+        "2"->"2.2"
+      )))
+      index getItem "2.1"  should be (Some("2.1"))
+      index getItem "2.2"  should be (Some("2.2"))
+      index getItem "3"  should be (Some(Map(
+        "1"->Map(
+          "1"->"3.1.1",
+          "2"->"3.1.2"
+        ),
+        "2"->"3.2"
+      )))
+      index getItem "3.1"  should be (Some(Map(
+        "1"->"3.1.1",
+        "2"->"3.1.2"
+      )))
+      index getItem "3.1.1"  should be (Some("3.1.1"))
+      index getItem "3.1.2"  should be (Some("3.1.2"))
+      index getItem "3.2"  should be (Some("3.2"))
+    }
   }
 }
